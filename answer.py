cf1ab5a8c3ee70d54d10a2021b356f3b3ebf25a4
RoyzellW <royzellwalton@gmail.com>
2018-11-16 09:53:23 -0600

    Done

---
+++ answer.py
@@ -0,0 +1,53 @@
+
+one = "1"
+two = "10"
+three = "11"
+four = "100"
+five = "101"
+six = "110"
+seven = "111"
+eight = "1000"
+# decimal
+oneDot = "1.0"
+twoDot = "2.0"
+threeDot = "3.0"
+fourDot = "4.0"
+fiveDot = "5.0"
+sixDot = "6.0"
+sevenDot = "7.0"
+eightDot = "8.0"
+
+binaryArray = [one, two, three, four, five, six, seven, eight]
+decimalArracy = [oneDot, twoDot, threeDot, fourDot, fiveDot, sixDot, sevenDot, eightDot]
+
+
+
+# for bin in binaryArray:
+#     for dec in decimalArracy:
+#         print("Binary:Decimal = " + bin + " : " + dec)
+
+if(binaryArray[0]) != decimalArracy[0]:
+    print("Binary:Decimal = " + binaryArray[0] + " : " + decimalArracy[0])
+
+if(binaryArray[1]) != decimalArracy[1]:
+    print("Binary:Decimal = " + binaryArray[1] + " : " + decimalArracy[1])
+
+if(binaryArray[2]) != decimalArracy[2]:
+    print("Binary:Decimal = " + binaryArray[2] + " : " + decimalArracy[2])
+
+if(binaryArray[3]) != decimalArracy[3]:
+    print("Binary:Decimal = " + binaryArray[3] + " : " + decimalArracy[3])
+
+if(binaryArray[4]) != decimalArracy[4]:
+    print("Binary:Decimal = " + binaryArray[4] + " : " + decimalArracy[4])
+
+if(binaryArray[5]) != decimalArracy[5]:
+    print("Binary:Decimal = " + binaryArray[5] + " : " + decimalArracy[5])
+
+if(binaryArray[6]) != decimalArracy[6]:
+    print("Binary:Decimal = " + binaryArray[6] + " : " + decimalArracy[6])
+
+if(binaryArray[7]) != decimalArracy[7]:
+    print("Binary:Decimal = " + binaryArray[7] + " : " + decimalArracy[7])
+
+
